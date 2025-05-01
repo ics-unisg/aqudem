764f48c4c36d33ab9314c3a5ac557a81101f5923
Aaron Kurz <aaronkurz@posteo.de>
2025-05-01 15:26:53 +0200

    Update version

--- package/aqudem/__init__.py
+++ package/aqudem/__init__.py
@@ -8,7 +8,7 @@ from .utils import (XESMissingTraceNameAttribute, XESSamplingFreqError,
 
 __author__ = """ICS, Universität St. Gallen"""
 __email__ = 'aaron.kurz@unisg.ch'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 __all__ = ["Context",
            "EventAnalysis",
