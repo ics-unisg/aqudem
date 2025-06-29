4fb194d04d05bbe10f2f6527adf7c42af12af227
Aaron Kurz <aaronkurz@posteo.de>
2025-06-29 18:28:42 +0200

    Imporve minor version since some update will change semantically, add HISTORY entry

--- package/aqudem/__init__.py
+++ package/aqudem/__init__.py
@@ -8,7 +8,7 @@ from .utils import (XESMissingTraceNameAttribute, XESSamplingFreqError,
 
 __author__ = """ICS, Universität St. Gallen"""
 __email__ = 'aaron.kurz@unisg.ch'
-__version__ = '0.2.2'
+__version__ = '0.3.0'
 
 __all__ = ["Context",
            "EventAnalysis",
